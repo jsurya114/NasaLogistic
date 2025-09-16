1d90e08bfaeb64e14c0e0c4790f5d3e562249ec5
Yadhu Krishna <yadhu808krishna@gmail.com>
2025-09-16 21:18:36 +0530

    feat/Slice for new user

--- Frontend/src/components/AddUsers.jsx
+++ Frontend/src/components/AddUsers.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import Header from '../reuse/Header';
+import Nav from '../reuse/Nav';
 
 const AddUsers = () => {
   return (
-    <div>
-      
+    <div className="min-h-screen bg-gray-100 text-gray-900 font-poppins">
+        <Header/>
+        
+        <Nav/>
     </div>
-  )
+    )
 }
 
 export default AddUsers;
