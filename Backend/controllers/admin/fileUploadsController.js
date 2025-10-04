962a2f6b77dfea1cb601d42f153eabf935bb40ed
starlit-comet <3akshaiachu3@gmail.com>
2025-10-04 13:45:12 +0530

    fixed bug in excel file upload, implemened excel file delete after data extraction.

--- Backend/controllers/admin/fileUploadsController.js
+++ Backend/controllers/admin/fileUploadsController.js
@@ -5,21 +5,11 @@ import HttpStatus from "../../utils/statusCodes.js";
 import XLSX from "xlsx";
 import { ExcelFileQueries } from "../../services/admin/excelFileQueries.js";
 import { table } from "console";
+import { fsync,unlink } from "fs";
 // import { AdminDashboardQueries } from "../../services/admin/dashboardQueries.js";
-const RouteObj = Object.freeze({
-  "DFW 036-1": "36A",
-  "DFW 041 A-1": "41A",
-  "DFW 039-1": "39A",
-  "DFW 037-1": "37A",
-  "DFW 034-A-1": "34A",
-  "DFW 041 B-1": "41B",
-  "DFW 040-1": "40A",
-  "DFW 035-1": "35A",
-  "DFW 034 B-1": "34B",
-  "DFW 038-1": "38A",
-});
+
 // const sheetName = "dup";
-const sheetName = "dup";
+const sheetName = "result";
 export const getUpdatedTempDashboardData = async(req,res)=>{
   try {
     const result = await ExcelFileQueries.getTempDashboardData()
@@ -35,26 +25,16 @@ export const DailyExcelUpload = async (req, res) => {
     console.log(req.body)
     const chosenDate = req.body?.date
     if(!chosenDate) return res.status(HttpStatus.BAD_REQUEST).json({success:false,message:"No Date Chosen"})
-    // if(!req.file) {
-    //     return res.status(HttpStatus.BAD_REQUEST).json({success:false,message:'NO file uploaded'})
-    // }
-    // const fileName = req.file
-    const fileData = {
-      fieldname: "file",
-      originalname: "dms_tmp_route_parcel_info_175306440034lsx",
-      encoding: "7bit",
-      mimetype:
-        "application/vnd.openxmlformats-officedocum.spreadsheetml.sheet",
-      destination: "uploads/",
-      filename: "1758729143472.xlsx",
-      path: "uploads\\1758729143472.xlsx",
-      size: 250675,
-    };
-    // console.log(fileData, "uploaded file data");
+    if(!req.file) {
+        return res.status(HttpStatus.BAD_REQUEST).json({success:false,message:'NO file uploaded'})
+    }
+    const fileName = req.file
+ 
 
     // console.log("fileuploaded",req.file.filename);
-    const workbook = XLSX.readFile(fileData.path);
+    const workbook = XLSX.readFile(fileName.path);
     // const workbook = XLSX.readFile(fileData.path);
+    // console.log(workbook.SheetNames())
     const sheet = workbook.Sheets[sheetName];
     if (!sheet) {
       return res.status(400).json({ error: "Sheet named ... not found" });
@@ -75,7 +55,10 @@ export const DailyExcelUpload = async (req, res) => {
     await ExcelFileQueries.addEachDriversCount()
 
 
-    
+     unlink(fileName.path,(e)=>{
+      if(e) throw new Error(e)
+        console.log('excel file deleted')
+     })
     return res.status(statusCode.OK).json({ message: "code endeddd" });
   } catch (error) {
     console.error(error)
