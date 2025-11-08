a89eebfae8c558c770c41f58e8d44f1ffae59888
starlit-comet <3akshaiachu3@gmail.com>
2025-11-08 18:43:50 +0530

    fixed TTL in redis db

--- Backend/services/redis-jwt-service.js
+++ Backend/services/redis-jwt-service.js
@@ -2,7 +2,12 @@ import client from '../config/redis.js'
 
 export  function blackListToken(jti){
     console.log(jti,'-> added to blacklist')
-    client.set(jti,'revoked','EX',3600)
+    client.set(jti,'revoked')
+    client.expire(jti,3600)
+    client.ttl(jti)
+
+
+    
 }
 
 export  async function isTokenRevoked(jti){
