ff78fdaf928c8f9cecfa33de80d8f469554fd074
starlit-comet <3akshaiachu3@gmail.com>
2025-12-01 12:18:32 +0530

    fixing issue in journey editing in admin

--- Backend/services/admin/AjourneyQuery.js
+++ Backend/services/admin/AjourneyQuery.js
@@ -2,11 +2,11 @@ import pool from "../../config/db.js";
 const AdminJourneyQuery={
     getAllJourneys:async()=>{
        const query = ` 
-SELECT d.*, r.name AS route_name, dr.name AS driver_name
-FROM dashboard_data d 
-JOIN routes r ON d.route_id = r.id
-JOIN drivers dr ON d.driver_id = dr.id
-ORDER BY d.journey_date DESC, d.start_seq`;
+              SELECT d.*, r.name AS route_name, dr.name AS driver_name
+              FROM dashboard_data d 
+              JOIN routes r ON d.route_id = r.id
+              JOIN drivers dr ON d.driver_id = dr.id
+              ORDER BY d.journey_date DESC, d.start_seq`;
 
        
         const result = await pool.query(query)
@@ -76,6 +76,7 @@ let query = `
     `;
     const values = [start_seq,end_seq,route_id,id]
     const result = await pool.query(query,values)
+      console.log('querey updated in admin side, ' ,result.rows[0])
     return result.rows[0]
 
     },
